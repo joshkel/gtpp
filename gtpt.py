e5cdfc7065383327b3296cd870613e1a8d3e7865
Josh Kelley <joshkel@gmail.com>
2017-02-28 23:05:54 -0500

    Colorized test case output

--- gtpt.py
+++ gtpt.py
@@ -48,39 +48,57 @@ class Parser(object):
         self.current_test_case = None
         self.current_test_count = 0
         self.test_index = 0
+        self.current_fail_count = 0
+
+        self.in_raw_output = True
 
     def process(self, line):
         for h in self.handlers:
             if h(line):
+                self.in_raw_output = False
                 return True
+
+        if not self.in_raw_output:
+            self.in_raw_output = True
+            print()
 
     @handle(r'Running (\d+) tests? from (\d+) test cases?')
     def start(self, total_test_count, total_test_case_count):
-        self.total_test_count = total_test_count
-        self.total_test_case_count = total_test_case_count
+        self.total_test_count = int(total_test_count)
+        self.total_test_case_count = int(total_test_case_count)
 
-    @handle(r'\[-+\] (\d+) tests? from (.*)' + TIME_RE)
+    @handle(r'\[-+\] (\d+) tests? from (.*?)' + TIME_RE + '$')
     def start_stop_test_case(self, test_count, test_case, time=None):
         if not self.current_test_case:
             self.current_test_case = test_case
-            self.current_test_count = test_count
+            self.current_test_count = int(test_count)
+            self.current_fail_count = 0
             self.test_index = 0
             self.test_case_index += 1
             print(progress(self.test_case_index, self.total_test_case_count) + '   ' + test_case, end='')
         else:
+            if not self.current_fail_count:
+                print('\r' + progress(self.test_case_index, self.total_test_case_count) + Fore.GREEN + ' ✓ ' + test_case + Style.RESET_ALL, end='')
+            else:
+                print('\r' + progress(self.test_case_index, self.total_test_case_count) + Fore.RED + ' ✗ ' + test_case + ' - %i/%i failures' % (self.current_fail_count, self.current_test_count) + Style.RESET_ALL, end='')
+            if time:
+                print(' (%s ms)' % time)
+            else:
+                print()
             self.current_test_case = None
 
     @handle(r'\[ *RUN *\] (.*)\.(.*)')
     def start_test(self, test_case, test_count):
         pass
 
-    @handle(r'\[ *(OK|FAILED) *\] (.*)\.(.*)' + TIME_RE)
+    @handle(r'\[ *(OK|FAILED) *\] (.*)\.(.*?)' + TIME_RE + '$')
     def stop_test(self, status, test_case, test_count, time=None):
-        pass
+        if status == 'FAILED':
+            self.current_fail_count += 1
 
     @handle(r'^$')
     def blank_line(self):
-        if not self.current_test_case:
+        if self.current_test_case:
             return False
 
 
